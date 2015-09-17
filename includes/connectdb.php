23972a8a9c97b1dae17dfe405025382c7ae3aa94
jschouwstra <schouwstra.jelle@gmail.com>
2015-09-17 22:26:29 +0200

    Delete connectdb.php

--- includes/connectdb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = 'localhost';
-$database = 'pdo';
-$username = 'root';
-$password = 'FXExWHemDmbKq2yu';
-
-$dsn = "mysql:host=$host;dbname=$database";
-$db = new PDO($dsn, $username, $password);
